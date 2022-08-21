8e8156fbe25a7474a4a48624334719e11bcf54a1
ChatWithMePro <lidudu890890@163.com>
2022-08-21 19:52:43 +0800

    fix bugs

--- LogOutput.xaml.cs
+++ LogOutput.xaml.cs
@@ -1,31 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Panuon.UI.Silver;
+using System.Diagnostics;
+using System;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Android_App_Install
 {
     /// <summary>
     /// LogOutput.xaml 的交互逻辑
     /// </summary>
-    public partial class LogOutput : Window
+
+    public partial class LogOutput : WindowX
     {
+        public static LogOutput i;
         public LogOutput()
         {
+            i = this;
             InitializeComponent();
+            Log.Text = "等待日志输出...\n";
         }
-        public static void Install(string path)
+
+        public static string getLog = "";
+
+        public static async void Install(string path)
         {
-            
+            await Task.Run(() =>
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "adb";
+                process.StartInfo.Arguments = @" install """ + path + @"""";
+                // 禁用操作系统外壳
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                i.Log.AppendText(process.StandardOutput.ReadToEnd());
+                i.Log.AppendText(process.StandardError.ReadToEnd());
+                process.WaitForExit();
+                process.Close();
+            });
+        }
+
+        public static string FastInstall(string path)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "adb";
+                process.StartInfo.Arguments = @" install """ + path + @"""";
+                // 禁用操作系统外壳
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                //i.Log.AppendText(process.StandardOutput.ReadToEnd());
+                //i.Log.AppendText(process.StandardError.ReadToEnd());
+                getLog += process.StandardOutput.ReadToEnd();
+                getLog += process.StandardError.ReadToEnd();
+                process.WaitForExit();
+                process.Close();
+                if (MainWindow.h.Progress.Value < MainWindow.h.Progress.Maximum - 10) MainWindow.h.Progress.Value += 10;
+            }
+            catch (Exception)
+            {
+                return "ERROR";
+            }
+            return getLog;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Diagnostics;
-using System;
-using System.IO;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Forms;
 using Panuon.UI.Silver;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
 
 namespace Android_App_Install
 {
@@ -12,8 +15,10 @@ namespace Android_App_Install
     /// </summary>
     public partial class MainWindow : WindowX
     {
+        public static MainWindow h;
         public MainWindow()
         {
+            h = this;
             InitializeComponent();
         }
 
@@ -31,7 +36,7 @@ namespace Android_App_Install
         }
         public void LoadPathAPK(object sender, RoutedEventArgs e)
         {
-            if (SelectList.Items.Count != 0) SelectList.Items.Clear();
+            if (SelectList.Items.Count != 0) SelectList.Items.Refresh();
             if (!string.IsNullOrEmpty(APKPath.Text)) if (Directory.Exists(APKPath.Text))
             {
                 SelectList.ItemsSource = Directory.GetFiles(APKPath.Text, "*.apk");
@@ -43,8 +48,18 @@ namespace Android_App_Install
 
         private void Add(object sender, RoutedEventArgs e)
         {
-            if (SelectList.SelectedItem != null) InstallList.Items.Add(SelectList.SelectedItem);
+            bool put = true;
+            if (SelectList.SelectedItem != null)
+            {
+                for (int i = 0; i < InstallList.Items.Count; i++)
+                {
+                    if (SelectList.SelectedItem == InstallList.Items.GetItemAt(i)) put = false;
+                }
+                if (put) InstallList.Items.Add(SelectList.SelectedItem);
+            }
         }
+
+
 
         private void AddAll(object sender, RoutedEventArgs e)
         {
@@ -64,15 +79,74 @@ namespace Android_App_Install
 
         private void Install(object sender, RoutedEventArgs e)
         {
-            LogOutput.Install(InstallList.SelectedItem.ToString());
+            if (InstallList.SelectedItem != null)
+            {
+                Stats.Text = "创建窗口";
+                Progress.Value = 0;
+                new LogOutput().Show();
+                LogOutput.i.Log.Text = "";
+                Progress.Value = 100;
+                Stats.Text = "开始安装";
+                Progress.Value = 0;
+                LogOutput.Install(InstallList.SelectedItem.ToString());
+                Stats.Text = "安装完成";
+                Progress.Value = 100;
+            }
         }
+
+        public bool MultiThreadMode = false;
+
+        public string IAO = "";
 
         private void InstallAll(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < SelectList.Items.Count; i++)
+            if (InstallList.Items.Count != 0)
             {
-                LogOutput.Install(SelectList.Items.GetItemAt(i).ToString());
+                if (MultiThreadMode)
+                {
+                    Stats.Text = "创建窗口";
+                    new LogOutput().Show();
+                    LogOutput.i.Log.Text = "";
+                    Progress.Value = 100;
+                    Progress.Value = 0;
+                    Stats.Text = "创建线程";
+                    for (int i = 0; i < InstallList.Items.Count; i++)
+                    {
+                        int sbBug = i;
+                        Thread thread = new Thread(() =>
+                        {
+                            IAO = LogOutput.FastInstall(InstallList.Items.GetItemAt(sbBug).ToString());
+                        });
+                        thread.SetApartmentState(ApartmentState.STA);
+                        thread.Start();
+                        if (Progress.Value < Progress.Maximum - 10) Progress.Value += 10;
+                    }
+                    if (IAO == "ERROR") MessageBoxX.Show("程序出现错误");
+                    else LogOutput.i.Log.AppendText(IAO);
+                }
+                else
+                {
+                    Stats.Text = "创建窗口";
+                    new LogOutput().Show();
+                    Stats.Text = "开始安装";
+                    for (int i = 0; i < InstallList.Items.Count; i++)
+                    {
+                        LogOutput.Install(InstallList.Items.GetItemAt(i).ToString());
+                    }
+                    Stats.Text = "安装完成";
+                }
             }
+        }
+
+        private void EnableMultiThread(object sender, RoutedEventArgs e)
+        {
+            MessageBoxX.Show("开启多线程模式会使安装速度变快，但可能出现无法预料的错误", "提示");
+            MultiThreadMode = true;
+        }
+
+        private void DisableMultiThread(object sender, RoutedEventArgs e)
+        {
+            MultiThreadMode = false;
         }
     }
 }
